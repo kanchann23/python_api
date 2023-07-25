cc08f9c1cf1bd4ad46d1c7ebf5b9c4ac7184a09e
kanchann23 <kanchanbora321@gmail.com>
2023-07-25 16:57:52 +0530

    /password updated

--- python_api.py
+++ python_api.py
@@ -1,42 +1,40 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-#These classes are necessary to handle HTTP requests and create an HTTP server.
+from passw import gen_pass
+
 class apiHandler(BaseHTTPRequestHandler):
-    #we will override its methods to define how the API responds to different types of HTTP requests.
     def do_GET(self):
-        # do_GET method is called when the server receives a GET request.
-
-
         if self.path == '/ping':
-            #it checks if the path of the incoming request is /ping.
-            # Send response status code
+            # Handle the /ping endpoint as before
             self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            message = "ping-pong"
+            self.wfile.write(message.encode())
 
-            # Send headers
+        elif self.path == '/password':
+            # Handle the /password endpoint
+            self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
-            # Send response content
-            message = "ping-pong"
+            # Generate a random password using the gen_pass function from the passw package
+            #password_length = 12  # You can set the desired password length here
+            generated_password = gen_pass()
+            message = "Generated password: {}".format(generated_password)
+
             self.wfile.write(message.encode())
-            # sends the response content back to the client
+
         else:
-            #  return 404 Not Found
             self.send_response(404)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(b'go to ping page')
+            self.wfile.write(b'404 Not Found')
 
 def run_server():
-    # Set the server address and port
     server_address = ('localhost', 8080)
-
-    # Create an HTTP server with the custom request handler
     httpd = HTTPServer(server_address, apiHandler)
     print('Starting server on http://{}:{}'.format(*server_address))
-
-    # Start the server to handle incoming requests
     httpd.serve_forever()
-
 
 run_server()
 
